e4ddfcf3f8e9be60e9019dbcc531925c60423e86
niharikacompro <niharikacomprotechnologies.com@gmail.com>
2025-01-21 16:44:08 +0530

    modularity and bugs

--- script.js
+++ script.js
@@ -5,16 +5,8 @@ let boardData = loadBoardData();
 const addlistbutton =document.getElementById('addListButton');
 
 saveBoardData(boardData);
-
-
-
-
-
-
-
-
 addlistbutton.addEventListener("click", function() {
-    handleAddList(boardData);  // Pass boardData explicitly here
+    handleAddList(boardData); 
 });
 
 renderBoard(boardData);
